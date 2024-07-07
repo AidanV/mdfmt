9891a3ca8bc97754464ea2f4b9d5b825cf8e4b60
AidanV <aidanvanduyne@gmail.com>
2024-07-07 14:12:19 -0700

    First test written

---
+++ fmt_test.go
@@ -0,0 +1,23 @@
+package nixfmt
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestFmt(t *testing.T) {
+	for i := 0; i < 1; i++ {
+		in, err := os.ReadFile(fmt.Sprintf("./testdata/input/%d.nix", i))
+		if err != nil {
+			panic(err)
+		}
+		out, err := os.ReadFile(fmt.Sprintf("./testdata/output/%d.nix", i))
+		if err != nil {
+			panic(err)
+		}
+        if string(in) != string(out) {
+          t.Error();
+        }
+	}
+}
